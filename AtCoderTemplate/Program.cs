c4d6f44f8c91179be9f337e2800c23f65de36219
TeruMiyake <miyake.ff@gmail.com>
2022-01-22 12:17:22 +0900

    プロジェクト ファイルを追加します。

---
+++ AtCoderTemplate/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO; // input.txt/output.txt IO 用
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AtCoderTemplate
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Solver sol = new Solver();
+            sol.test();
+        }
+    }
+
+    public class Solver
+    {
+        private System.Diagnostics.Stopwatch sw;
+        public void solve()
+        {
+            sw.Stop();
+            //int [] intArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            //int L = intArr[0]; int R = intArr[1];
+            //string S = Console.ReadLine();
+            int t = int.Parse(Console.ReadLine());
+            sw.Start();
+
+        }
+
+        public Solver()
+        {
+            sw = new System.Diagnostics.Stopwatch();
+        }
+
+        public void test()
+        {
+            while (true)
+            {
+                sw.Restart();
+                solve();
+                sw.Stop();
+                Console.WriteLine("Runtime " + sw.ElapsedMilliseconds + " ms");
+            }
+        }
+    }
+}
---
+++ LibraryForDotnetCore/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LibraryForDotnetCore
+{
+    public class Class1
+    {
+    }
+
+    public static class MyUtil
+    {
+        /// <summary>
+        ///  競プロ用自作便利機能集
+        /// </summary>
+        /// <param name="obj"></param>
+        public static void WriteType(this Object obj)
+        {
+            /// 受け取った変数の型の正式名称をコンソールに表示
+            Console.WriteLine(obj.GetType().FullName);
+        }
+
+        public static long CeilDivide(long a, long b)
+        {
+            return (a + b - 1) / b;
+        }
+    }
+}
